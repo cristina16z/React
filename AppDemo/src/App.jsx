ceb99a911632bf5d3fb8a5e13a19e47d8b38cf69
Cristina <2023_cristina.val@iticbcn.cat>
2025-04-03 18:46:42 +0200

    ✨ Map interaction
    Map interaction

--- AppDemo/src/App.jsx
+++ AppDemo/src/App.jsx
@@ -8,52 +8,34 @@ import { Usuario } from './Usuari.jsx'
 import { Cart } from './Components/Cart/Cart.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const llistaProductes = [{name:"Portàtil", preu:500, desc:"Portàtil Asus"},
+                          {name:"Auriculars", preu:50, desc:"Auriculars amb NC"},
+                          {name:"Ratolí", preu:20, desc:"Ratolí inalàmbric"}]
+
+  console.log(llistaProductes)
 
   return (
     <div>
+      
       <h1>Hello world</h1>
       <Usuario/>
       
 
       <section className='products-container'>
-        <Cart name="Portàtil" preu="500" desc="Portàtil Asus"/>
-        <Cart name="Auriculars" preu="50" desc="Auriculars amb NC"/>
-        <Cart name="Ratolí" preu="20" desc="Ratolí inalàmbric"/>
-     
+
+        {
+          llistaProductes.map((product, index) => (
+            <Cart key={index} name={product.name} preu={product.preu} desc={product.desc}/>
+          ))
+
+        }
+        
+       
 
       </section>
-      
+
     </div>
-
-   
-    
-
-
-    /*
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>Hello world</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>*/
   )
 }
 
